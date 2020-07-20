f031cc1035ba914141d5a896ffb1ae89b23fc498
Rishabh Priyadarshi <rishabhpriyadarshi.discovery@gmail.com>
2020-07-21 03:30:05 +0530

    Lyrics Search

--- lyrics-search/app.js
+++ lyrics-search/app.js
@@ -1,8 +1,28 @@
-const apiURL = "https://api/lyrics.ovh";
+const apiURL = "https://api.lyrics.ovh";
 
 // Elements
-
 const form = document.getElementById("form");
 const search = document.getElementById("search");
 const result = document.getElementById("result");
 const more = document.getElementById("more");
+
+//Search funtion
+
+async function searchSongs(term) {
+  const res = await fetch(`${apiURL}/suggest/${term}`);
+  const data = await res.json();
+  console.log(data);
+}
+
+// Event listeners
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const searchTerm = search.value.trim();
+
+  if (!searchTerm) {
+    alert("Please type a song or artist name");
+  } else {
+    searchSongs(searchTerm);
+  }
+});
