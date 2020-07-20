6971b5ef011703c46e6498f290842e7e5a11cf94
Rishabh Priyadarshi <rishabhpriyadarshi.discovery@gmail.com>
2020-07-21 03:13:57 +0530

    Lyrics Seach

---
+++ lyrics-search/app.js
@@ -0,0 +1,8 @@
+const apiURL = "https://api/lyrics.ovh";
+
+// Elements
+
+const form = document.getElementById("form");
+const search = document.getElementById("search");
+const result = document.getElementById("result");
+const more = document.getElementById("more");
